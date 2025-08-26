69bc8352e72eccd4c7cbc09964ded91127ab6446
urwashiverma2006-cell <urwashiverma2006@gmail.com>
2025-08-26 09:35:05 +0530

    Initial Commit 1.5

--- model/train.py
+++ model/train.py
@@ -3,20 +3,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 
-#Load the dataset
+#Load dataset
 data = pd.read_csv('data/iris.csv')
 
-#preprocess the dataset
+# Preprocess the dataset
 X = data.drop('species', axis=1)
 y = data['species']
 
-#split the data into training and test sets
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+# split the data into training and test sets 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 42)
 
-
-#train a randomforest model
-model=RandomForestClassifier(n_estimators=100,random_state=42)
-model.fit(X_train,y_train)
+# train a randomforest model
+model = RandomForestClassifier(n_estimators= 100, random_state=42)
+model.fit(X_train, y_train)
 
 #save the model
-joblib.dump(model,'model/iris_model.pkl')
+joblib.dump(model, 'model\iris_model.pkl')\
