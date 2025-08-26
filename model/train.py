9175ae5e0e5ab76faa7abc035fbedf172c236150
urwashiverma2006-cell <urwashiverma2006@gmail.com>
2025-08-26 09:17:09 +0530

    Initial Commit 1.3

--- model/train.py
+++ model/train.py
@@ -4,11 +4,11 @@ from sklearn.ensemble import RandomForestClassifier
 import joblib
 
 #Load the dataset
-data=pd.read_csv('data/iris.csv')
+data = pd.read_csv('data/iris.csv')
 
 #preprocess the dataset
-X= data.drop('species',axis=1)
-y=data['species']
+X = data.drop('species', axis=1)
+y = data['species']
 
 #split the data into training and test sets
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
