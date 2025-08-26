e8871bc78bdef207c49fa842f0417acc5aeb5514
urwashiverma2006-cell <urwashiverma2006@gmail.com>
2025-08-26 09:26:25 +0530

    Initial Commit 1.4

--- model/train.py
+++ model/train.py
@@ -13,6 +13,7 @@ y = data['species']
 #split the data into training and test sets
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
 
+
 #train a randomforest model
 model=RandomForestClassifier(n_estimators=100,random_state=42)
 model.fit(X_train,y_train)
