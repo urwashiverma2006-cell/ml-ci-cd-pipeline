19573232db709924c8150ecd3968338ff240690e
urwashiverma2006-cell <urwashiverma2006@gmail.com>
2025-08-26 10:20:59 +0530

    Initial Commit 1.12

--- model/evaluate.py
+++ model/evaluate.py
@@ -7,8 +7,8 @@ import joblib
 data=pd.read_csv('data/iris.csv')
 
 #Preprocess the dataset
-X= data.drop('species',axis=1)
-y=data['species']
+X= data.drop('Species',axis=1)
+y=data['Species']
 
 #Split the data into training and test sets
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
