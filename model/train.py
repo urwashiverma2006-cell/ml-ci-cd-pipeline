4f937b1b134f4ffe50bed9fa7d29de19f5bc4b05
urwashiverma2006-cell <urwashiverma2006@gmail.com>
2025-08-26 09:03:44 +0530

    Initial Commit 1.1

---
+++ model/train.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+#Load the dataset
+data=pd.read_csv('data/iris.csv')
+
+#preprocess the dataset
+X= data.drop('species',axis=1)
+y=data['species']
+
+#split the data into training and test sets
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+
+#train a randomforest model
+model=RandomForestClassifier(n_estimators=100,random_state=42)
+model.fit(X_train,y_train)
+
+#save the model
+joblib.dump(model,'model/iris_model.pkl')
---
+++ tests/test_model.py
@@ -0,0 +1,13 @@
+import unittest
+import joblib
+from sklearn.ensemble import RandomForestClassifier
+
+class TestModelTraining(unittest.TestCase):
+	def test_model_training(self):
+		model=joblib.load('model/iris_model.pkl')
+		self.assertIsInstance(model, RandomForestClassifier)
+		self.assertGreaterEqual(len(model.feature_importances_),4)
+
+
+if__name__=='__main__':
+	unittest.main()
