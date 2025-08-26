0f6fefaf0c425ae27ded28baf6165e638197a41b
urwashiverma2006-cell <urwashiverma2006@gmail.com>
2025-08-26 10:39:09 +0530

    Initial Commit 1.4

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,10 +4,9 @@ from sklearn.ensemble import RandomForestClassifier
 
 class TestModelTraining(unittest.TestCase):
 	def test_model_training(self):
-		model=joblib.load('model/iris_model.pkl')
+		model = joblib.load('model/iris_model.pkl')
 		self.assertIsInstance(model, RandomForestClassifier)
-		self.assertGreaterEqual(len(model.feature_importances_),4)
+		self.assertGreaterEqual(len(model.feature_importances_), 4)
 
-
-if__name__=='__main__':
+if__name__== '__main__':
 	unittest.main()
